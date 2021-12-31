d963c33b8592460f7981c3870872889c7a6d0ed3
bhuvanesh <bhuvanesh.coder@gmail.com>
2021-12-31 18:59:26 +0530

    fix

--- playground/views.py
+++ playground/views.py
@@ -132,7 +132,7 @@ class ApplicationView(APIView):
 
 # pagination config
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 5
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
